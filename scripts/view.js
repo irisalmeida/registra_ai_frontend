118b6b68bfb52f93f47e635484194bd8d0a517a4
Iago Tito Oliveira <iago.tito@pagar.me>
2024-06-01 17:16:05 -0300

    Split html in template files

--- scripts/view.js
+++ scripts/view.js
@@ -1,36 +1,47 @@
 import { isLogged, openLoginWindow, logout } from "./auth.js";
-import { getUserData } from "./user.js";
+import { getUserData, registerGain, registerExpense } from "./api.js";
 
 let $main = document.getElementById("main");
 
-export function loadHomeView() {
+export async function loadHomeView() {
   if (isLogged()) {
-    loadLoggedView();
+    await loadLoggedView();
   } else {
-    loadNotLoggedView();
+    await loadNotLoggedView();
   }
 }
 
-export function loadNotLoggedView() {
-  let $template = document.getElementById("notLoggedView");
-  $main.innerHTML = $template.innerHTML;
+async function fetchTemplate(url) {
+  const response = await fetch(url);
+  const text = await response.text();
+  const temp = document.createElement("div");
+  temp.innerHTML = text;
+  return temp.querySelector("template");
+}
+
+export async function loadNotLoggedView() {
+  const template = await fetchTemplate("templates/notLoggedView.html");
+  $main.innerHTML = template.innerHTML;
 
   let $loginButton = document.getElementById("loginButton");
   $loginButton.addEventListener("click", openLoginWindow);
 }
 
 export async function loadLoggedView() {
-  let $template = document.getElementById("loggedView");
-  $main.innerHTML = $template.innerHTML;
+  const template = await fetchTemplate("templates/loggedView.html");
+  $main.innerHTML = template.innerHTML;
 
   let $logoutButton = document.getElementById("logoutButton");
   $logoutButton.addEventListener("click", logout);
 
+  let userData = await getUserData();
   let $userName = document.getElementById("userName");
   let $userImage = document.getElementById("userImage");
-
-  let userData = await getUserData();
-
   $userName.textContent = userData["username"];
   $userImage.src = userData["profile_pic"];
+
+  let $gainButton = document.getElementById("gainButton");
+  let $expenseButton = document.getElementById("expenseButton");
+  $gainButton.addEventListener("click", registerGain);
+  $expenseButton.addEventListener("click", registerExpense);
 }
