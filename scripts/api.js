ac733479fde61d60fe0b91e0529a22de0aef4613
Iago Tito Oliveira <iago.tito@pagar.me>
2024-06-08 16:48:41 -0300

    Send register requests to backend

---
+++ scripts/api.js
@@ -0,0 +1,42 @@
+const API_URL = "https://127.0.0.1:8001";
+
+async function request(method, route, data = null) {
+  let options = {
+    method: method,
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+  };
+
+  if (data) {
+    options.body = JSON.stringify(data);
+  }
+
+  try {
+    const response = await fetch(API_URL + route, options);
+    let body = await response.json()
+    if (!response.ok) {
+      console.error(body);
+      return null;
+    }
+    return body;
+  } catch (err) {
+    console.error('Fetch error:', err);
+  }
+}
+
+export async function getUserData() {
+  return await request("GET", "/user_data");
+}
+
+export async function register(type, amount, description) {
+  let body = {
+    "amount": amount,
+    "description": description
+  }
+
+  let res = await request("POST", `/${type}`, body);
+
+  console.log(res);
+
+  return res
+}
--- scripts/view.js
+++ scripts/view.js
@@ -1,5 +1,5 @@
 import { isLogged, openLoginWindow, logout } from "./auth.js";
-import { getUserData, registerGain, registerExpense } from "./api.js";
+import { getUserData, register } from "./api.js";
 
 let $main = document.getElementById("main");
 
@@ -31,17 +31,80 @@ export async function loadLoggedView() {
   const template = await fetchTemplate("templates/loggedView.html");
   $main.innerHTML = template.innerHTML;
 
-  let $logoutButton = document.getElementById("logoutButton");
-  $logoutButton.addEventListener("click", logout);
+  document.getElementById("logoutButton").addEventListener("click", logout);
 
   let userData = await getUserData();
-  let $userName = document.getElementById("userName");
-  let $userImage = document.getElementById("userImage");
-  $userName.textContent = userData["username"];
-  $userImage.src = userData["profile_pic"];
+  document.getElementById("userName").textContent = userData["username"];
+  document.getElementById("userImage").src = userData["profile_pic"];
 
-  let $gainButton = document.getElementById("gainButton");
-  let $expenseButton = document.getElementById("expenseButton");
-  $gainButton.addEventListener("click", registerGain);
-  $expenseButton.addEventListener("click", registerExpense);
+  let $gainAmount = document.getElementById("gainAmount");
+  let $expenseAmount = document.getElementById("expenseAmount");
+  $gainAmount.addEventListener("input", function(event) {
+    $gainAmount.classList.remove("is-danger");
+    formatMoneyInput(event);
+  });
+  $expenseAmount.addEventListener("input", function(event) {
+    $expenseAmount.classList.remove("is-danger");
+    formatMoneyInput(event);
+  });
+
+  let $gainDescription = document.getElementById("gainDescription");
+  let $expenseDescription = document.getElementById("expenseDescription");
+
+  document.getElementById("gainButton").addEventListener("click", function() {
+    let amountStr = $gainAmount.value;
+    let description = $gainDescription.value;
+    let amount = brlStringToFloat(amountStr);
+
+    if (amount === 0) {
+      $gainAmount.classList.add("is-danger")
+      return
+    }
+    $gainAmount.classList.remove("is-danger");
+    $gainAmount.value = "R$ 0,00";
+    $gainDescription.value = "";
+
+    register("gain", amount, description);
+  });
+
+  document.getElementById("expenseButton").addEventListener("click", function() {
+    let amountStr = $expenseAmount.value;
+    let description = $expenseDescription.value;
+    let amount = brlStringToFloat(amountStr);
+
+    if (amount === 0) {
+      $expenseAmount.classList.add("is-danger")
+      return
+    }
+    $expenseAmount.classList.remove("is-danger");
+    $expenseAmount.value = "R$ 0,00";
+    $expenseDescription.value = "";
+
+    register("expense", amount, description);
+  });
+}
+
+function brlStringToFloat(brlString) {
+    // Remove the "R$" prefix
+    brlString = brlString.replace("R$", "").trim();
+    // Remove thousand separators
+    brlString = brlString.replace(/\./g, "");
+    // Replace the decimal comma with a dot
+    brlString = brlString.replace(",", ".");
+    // Convert to float
+    return parseFloat(brlString);
+}
+
+function formatMoneyInput(event) {
+  function formatMoney(value) {
+    let integerPart = value.replace(/^0+/, '');
+    let decimalPart = integerPart.length <= 2 ? integerPart.padStart(2, '0') : integerPart.slice(-2);
+    let formattedInteger = integerPart.length <= 2 ? '0' : integerPart.slice(0, -2).replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    return `${formattedInteger},${decimalPart}`;
+  }
+  let inputValue = event.target.value;
+  let numericValue = inputValue.replace(/\D/g, '');
+  let formattedMoney = formatMoney(numericValue);
+  let amount = `R$ ${formattedMoney}`
+  event.target.value = amount;
 }
